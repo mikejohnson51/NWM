c2f608288bc185b72ba3dd219c307e25b25da4a5
mikejohnson51 <jmj00@umail.ucsb.edu>
2017-07-12 18:38:43 -0500

    Add build.csv

---
+++ NWM/R/build_csv.R
@@ -0,0 +1,159 @@
+#' Exporting Timeseries Data for NWM data for select COMIDs using the NWM package
+#' 
+#' This function subsets a folder of temporal .nc data to a spatial list of COMIDs and returns a .csv of time series by COMID.
+#' If the user wants all temporal slices only the first three parameters are needed. Otherwise start.date, start.time, end.date, 
+#' and end.time can be used to refine the data that is returned.
+#' 
+#' @param 
+#' folder a path to a folder with netcdf files. If not yet obtained the #' Exporting Timeseries Data for NWM data for select COMIDs
+#' 
+#' This function subsets a folder of temporal .nc data to a spatial list of COMIDs and returns a .csv of time series by COMID.
+#' If the user wants all temporal slices only the first three parameters are needed. Otherwise start.date, start.time, end.date, 
+#' and end.time can be used to refine the data that is returned.
+#' 
+#' @param 
+#' folder a path to a folder with netcdf files. If not yet obtainded consider: \code{\link{get_server}} 
+#' @param
+#' start.date in format 'YYYY-MM-DD'. If left empty the first timeseries entry will default first day in the folder.
+#' @param 
+#' start.time in format 'HH" If left empty the first time series entry will default to the first file in the folder.
+#' @param
+#' end.date in format 'YYYY-MM-DD' If left empty the first timeseries entry will default last day in the folder.
+#' @param 
+#' end.time in format 'HH' If left empty the first time series entry will default to the last file in the folder.
+#' @param 
+#' COMID(s) of interest can be a single number or a list.
+#' If not yet determined consider \code{\link{get_COMIDs}} 
+#' @param 
+#' regions.name The name of the region, this will be used to name the output files
+#' @return 
+#' A .csv of flow records for the given time period for each given COMID. This file will be stored in the Output subfolder and will be named according to the dates and region provided
+#' @author 
+#' Mike Johnson and Jim Coll
+#' @examples 
+#' 
+#' Example 1) Return all time slices in a folder: \cr
+#' 
+#' build_csv(folder = "F:/johnson/NWM_DATA/NetCDFs/Current",  regions.name = "Onion Creek", comids = comids)
+#' 
+#' Example 2) Return a subset of time slices in a folder \cr 
+#' 
+#' build_csv(folder = "F:/johnson/NWM_DATA/complete2014_retro", \cr
+#' start.date = "2014-01-01", start.time = "00", \cr
+#' end.date = "2014-10-31",end.time= "23",  \cr
+#' regions.name = "Onion Creek", comids = comids.onion.creek)
+#' @export 
+#' 
+#' @param
+#' start.date in format 'YYYY-MM-DD'. If left empty the first timeseries entry will default first day in the folder.
+#' @param 
+#' start.time in format 'HH" If left empty the first time series entry will default to the first file in the folder.
+#' @param
+#' end.date in format 'YYYY-MM-DD' If left empty the first timeseries entry will default last day in the folder.
+#' @param 
+#' end.time in format 'HH' If left empty the first time series entry will default to the last file in the folder.
+#' @param 
+#' COMID(s) of interest can be a single number or a list.
+#' If not yet determined consider \code{\link{get_COMIDs}} 
+#' @param 
+#' regions.name The name of the region, this will be used to name the output files
+#' @return 
+#' A .csv of flow records for the given time period for each given COMID. This file will be stored in the Output subfolder and will be named according to the dates and region provided
+#' @author 
+#' Mike Johnson and Jim Coll
+#' @examples 
+#' 
+#' Example 1) Return all time slices in a folder: \cr
+#' 
+#' build_csv(folder = "F:/johnson/NWM_DATA/NetCDFs/Current",  regions.name = "Onion Creek", comids = comids)
+#' 
+#' Example 2) Return a subset of time slices in a folder \cr 
+#' 
+#' build_csv(folder = "F:/johnson/NWM_DATA/complete2014_retro", \cr
+#' start.date = "2014-01-01", start.time = "00", \cr
+#' end.date = "2014-10-31",end.time= "23",  \cr
+#' regions.name = "Onion Creek", comids = comids.onion.creek)
+#' @export
+
+build_csv = function(folder = 'current', start.date = NULL, start.time = NULL, end.date = NULL, end.time = NULL, comids, regions.name = "OnionCreek"){
+  
+  # Getting indexes -----------------------------------------------------
+  
+  if (folder == 'current'){
+    folder = paste0(getwd(), "/NetCDFs/Current")
+  }else{
+    folder = folder
+  }
+  
+  files = dir(folder)
+  
+  if ( is.null(start.date) ){
+    start.index = 1
+    start.date = gsub("-","",Sys.Date())
+  }else{
+    start.date = gsub("-", "", start.date)
+    fileOne = paste0(start.date)
+    start.index = grep(fileOne, files)
+  }
+  
+  if (is.null(end.date)){
+    end.index = length(files)
+    end.date = start.date
+  }else{
+    end.date = gsub("-", "", end.date)
+    end.index = grep(end.date, files)
+  }
+  
+  if(is.null(start.time)){
+    start.time = NULL
+  }else{
+    start.time = paste0("t",start.time)
+    end.index = grep()
+  }
+  
+  files = files[start.index:end.index]
+  
+  
+  nwm.flow = matrix(NA, ncol = length(files), nrow = length(comids))
+  
+  nc <- nc_open(filename = paste0(folder,"/", files[[1]]))
+  vars <- nc$var
+  start <- vector(mode = "numeric", length(comids))
+  
+  for (i in 1:length(comids)){
+    start[i] = which(vars$streamflow$dim[[1]]$vals == comids[i], arr.ind = TRUE)
+  }
+  
+  nc_close(nc)
+  
+  
+  
+  for (i in 1:length(files)) {
+    nc = nc_open(filename = paste0(folder,"/", files[i]))
+    
+    for(j in 1:length(comids)){
+      nwm.flow[j,i] = ncvar_get( nc, varid = "streamflow", start = c(start[j]), count = c(1))
+    } 
+    
+    nc_close(nc)
+    
+  }
+  
+  rownames(nwm.flow) = comids
+  colnames(nwm.flow) = substr(files, 1, 12)
+  
+  nwm.flow = nwm.flow * 35.3147
+  
+  regions.name = gsub(" ", "", regions.name)
+  
+  if(start.date == end.date){end.date = NULL}
+  
+  write.csv(nwm.flow, paste0(getwd(),"/Output/",regions.name,"_",start.date,"_", end.date, ".csv"))
+  
+  
+}
+
+
+
+
+
