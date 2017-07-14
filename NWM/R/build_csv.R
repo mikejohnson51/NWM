d8e15b2df8a2d37c8c53552df43e90f7a00c8da4
mikejohnson51 <jmj00@umail.ucsb.edu>
2017-07-13 19:25:57 -0500

    bueller...

--- NWM/R/build_csv.R
+++ NWM/R/build_csv.R
@@ -79,8 +79,8 @@
 build_csv = function(folder = 'current', start.date = NULL, start.time = NULL, end.date = NULL, end.time = NULL, comids, regions.name = "OnionCreek"){
   
   # Getting indexes -----------------------------------------------------
-    folder = 'current'; start.date = NULL; start.time = NULL; end.date = NULL; end.time = NULL; comids; regions.name = "OnionCreek"
-    comids = NC_comids; regions.name = "NC"
+    #folder = 'current'; start.date = NULL; start.time = NULL; end.date = NULL; end.time = NULL; comids; regions.name = "OnionCreek"
+    #comids = NC_comids; regions.name = "NC"
   
   if (folder == 'current'){
     folder = paste0(getwd(), "/NetCDFs/Current"
