c7a1a9596a8c7c81823ad79445eb36f77f4ccd70
mikejohnson51 <jmj00@umail.ucsb.edu>
2017-07-13 19:50:16 -0500

    changes

--- NWM/R/viz_current.R
+++ NWM/R/viz_current.R
@@ -1,3 +1,54 @@
+#' Visualize Current Streamflow Forecasts
+#' 
+#' Automatically generates a labeled visulization of hydrogrpahy at a point and porportionalized flow records.
+#' 
+#' @details 
+#' Each time the function is executed a subfolder mirroring the csv files name will be generated in the "Images" subfolder. 
+#' An addtion subfolder within this folder will be created named after the 'type' entered. \cr \cr
+#'  
+#' Each time the function is executed an additional subfolder will be created and numbered according to t
+#' he number of times
+#' the function has been executed for that type. \cr \cr
+#' 
+#' If all three types are executed twice the Images folder will have a subflder named after the csv file. In this folder will three subfolders: "hydrograph", "flows" and "combo".
+#' In each of these will be two subfolders named ex: ('type'1), ('type"2).
+#' 
+#' @seealso 
+#' \code{\link{build_csv}}
+#' 
+#' @seealso 
+#' \code{\link{get_COMIDS}}
+#' @seealso 
+#' \code{\link{get_current}}
+#' 
+#' @param 
+#' 
+#' @param 
+#' COMIDs requires a single COMID. See \code{\link{get_COMIDS}}
+#' @param 
+#' catchment_path path name to NHDPlusV2 catchments shapefile (.shp)
+#' @param 
+#' flowlines_path path name to NHDPlusV2 flowlines shapefile (.shp)
+#' @param 
+#' region a string of the regions name
+#' 
+#' @return 
+#' Images of the specified type to the "Images/" subfolder
+#' @author 
+#' Mike Johnson and Jim Coll
+#' @examples 
+#' 
+#' Return hydrograph
+#' 
+#' viz_csv(type = "hydrograph", COMIDs = comids[1:25], csv ="OnionCreek_20170711_20170711.csv",
+#' catchment_path = "/Users/mikejohnson/Desktop/Tester/Flowlines/Catchments/catchments.shp",
+#' flowlines_path = "/Users/mikejohnson/Desktop/Tester/Flowlines/Flowlines/Flowlines.shp")
+#' 
+#' @examples 
+#' viz_current = function(catchment_path, flowlines_path, COMID, region = "Santa Barbara")
+
+#' @export
+
   viz_current = function(catchment_path, flowlines_path, COMID, region){
     
   files = dir(paste0(getwd(),"/NetCDFs/Current"))
