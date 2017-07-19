440daeb04b03712e538b45321932a009ffea9149
mikejohnson51 <jmj00@umail.ucsb.edu>
2017-07-19 11:35:41 -0500

    //

--- NWM/living_flood_vingette.R
+++ NWM/living_flood_vingette.R
@@ -3,7 +3,8 @@
 
                       library(devtools); install_git("https://github.com/mikejohnson51/nwm1", "NWM"); library(NWM)         
 
-                                            setwd('/Users/mikejohnson/Desktop/Arizona/')
+                      
+                                            setwd('/Users/mikejohnson/Desktop/Austin/')
 
                                             
                                                           build_files()
@@ -12,15 +13,15 @@
 #=============================================== (Arizona flood ran in 7.5 min) ===============================================#
 
                                                           
-          living_flood_setup(flowlines_path = 'Geospatial/Flowlines/ForFred/AZ_Deaths.shp', hand.raster.needed = TRUE )
+          living_flood_setup(flowlines_path = 'Geospatial/Flowlines/OssomFlowLines/AuastinFlowlines.shp', hand.raster.needed = FALSE )
 
              
 #============================================== (Arizona flood ran in 41 seconds) =============================================#
 
              
-          living_flood_data(area_of_interest = "Arizona", flowlines_path = 'Geospatial/Flowlines/ForFred/AZ_Deaths.shp')
+          living_flood_data(area_of_interest = "Arizona", flowlines_path = 'Geospatial/Flowlines/OssomFlowLines/AuastinFlowlines.shp')
                                            
-                                           
+                                      
 
                                            
 #==============================================================================================================================#
