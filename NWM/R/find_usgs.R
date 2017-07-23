37ab8170d82146fd573894fa83218a90271cc46a
mikejohnson51 <jmj00@umail.ucsb.edu>
2017-07-23 17:19:17 -0500

    //

--- NWM/R/find_usgs.R
+++ NWM/R/find_usgs.R
@@ -5,13 +5,13 @@
 
 find_usgs = function(flowlines_path = NULL ){
 
-  #flowlines_path = '/Users/mikejohnson/Documents/R_NWM_tests/UCSB/Geospatial/Flowlines/nhdflowline_network.shp'
+  flowlines_path = '/Users/mikejohnson/Documents/R_NWM_tests/UCSB/Geospatial/Flowlines/nhdflowline_network.shp'
   
     flowlines = readOGR(flowlines_path)
      flowlines_84 = spTransform(flowlines, CRS("+proj=longlat +datum=WGS84"))
      
      
-        usgs1 = subset(usgs_gages, usgs_gages$lat_reachCent < flowlines_84@bbox[2,2])
+        usgs1 = subset(test, usgs_gages$lat_reachCent < flowlines_84@bbox[2,2])
           usgs2 = subset(usgs1, usgs1$lat_reachCent > flowlines_84@bbox[2,1])
           usgs3 = subset(usgs2, usgs2$lon_reachCent > flowlines_84@bbox[1,1])
         usgs4 = subset(usgs3, usgs3$lon_reachCent < flowlines_84@bbox[1,2])
@@ -25,7 +25,7 @@ find_usgs = function(flowlines_path = NULL ){
     
     
     usgs_final = cbind(usgs4$feature_id, usgs4$site_no, usgs4$lat_reachCent, usgs4$lon_reachCent)
-      colnames(usgs_final) = c("USGSgage", "COMID", "lat", "long")
+      colnames(usgs_final) = c("COMID", "USGSgage","lat", "long")
       
       usgs_final = as.matrix(usgs_final)
     
@@ -33,21 +33,41 @@ find_usgs = function(flowlines_path = NULL ){
     sp = SpatialPoints(coors, proj4string = CRS("+proj=longlat +datum=WGS84"))
     
     
+    num <- usgs_final[,2]  # site number
+    nam <- usgs_final[,1] # local site name
+    url <- sprintf("https://waterdata.usgs.gov/nwis/inventory/?site_no=%s", num)
+    url_call = paste0('<a href=', url,'>',num,"</a>")
+    pop <- sprintf("COMID: %s
+                   Site No: %s ",
+                   nam, url_call, num)
     
     m = leaflet() %>%
-      
-      addProviderTiles(providers$CartoDB.Positron) %>%
+      addTiles(group = "OSM") %>%
+      addProviderTiles(providers$CartoDB.Positron, group = "CartoDB") %>%
       
       addMarkers(data = usgs_final,lng= usgs_final[,4], lat = usgs_final[,3],
-                 popup = paste(sep ="<br/>" , "USGS Gage Number: ", usgs_final[,1],
-                          "COMID: ", usgs_final[,2]
-                          ))%>%
+          popup = pop, group = "USGS Stations")%>%
   
-       addPolylines(data = flowlines_84, color = 'blue', weight = flowlines_84$streamorde)%>%
+    addPolylines(data = flowlines_84, color = 'blue', weight = flowlines_84$streamorde, 
+                 label= label = paste0(paste0(flowlines_84@data$gnis_name),
+                                       paste0(" COMID:", flowlines_84$comid), group = "NHD Flowlines")%>%
+    
+      
+      addLayersControl(
+        baseGroups = c("OSM", "CartoDB"),
+        overlayGroups = c("USGS Stations", "NHD Flowlines"),
+        options = layersControlOptions(collapsed = FALSE)
+      ) %>%
+    
     addScaleBar("bottomleft")
     
     print(m)
+    
     return(usgs_final)
   
       
 }
+
+
+
+
