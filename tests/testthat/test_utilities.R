92d583b1cecec52cfe9e0da8c502d023b9b7056b
Mike Johnson <mike.johnson@geog.ucsb.edu>
2018-07-13 20:26:39 -0600

    Update test_utilities.R

--- tests/testthat/test_utilities.R
+++ tests/testthat/test_utilities.R
@@ -5,4 +5,21 @@ test_that("cida server throws correct errors", {
   expect_error(query_cida(AOI::getAOI(clip = list("UCSB", .1, .1)), type = "nhdflowline_network") , "O features found in this AOI.", fixed = TRUE)
 })
 
+test_that("utility_cida", {
+  data_check = !is.null(nwm::comids_all)
+  cida_sf = query_cida(AOI::getAOI(clip = list("UCSB", 10, 10)), type = "nhdflowline_network")
+  cida_sp = query_cida(AOI::getAOI(clip = list("UCSB", 10, 10)), type = "nhdflowline_network", spatial = T)
+  #error   = try(query_cida(AOI::getAOI(clip = list("UCSB", .1, .1))))
+
+  vec = c(
+        data_check,
+        any(class(cida_sf) == 'sf'),
+        any(class(cida_sp) == 'SpatialLinesDataFrame')
+        #class(error) == 'try-error'
+        )
+
+  check = any(!isTRUE(vec))
+  expect_true(check)
+})
+
 
